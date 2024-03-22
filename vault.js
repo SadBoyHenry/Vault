807f91321493ae4122309c0e973c3a1ce7b71394
SadBoyHenry <valorantmain243@gmail.com>
2024-03-22 13:25:47 -0400

    Adding previous workshop into my git hub repositorys

---
+++ vault.js
@@ -0,0 +1,10 @@
+//pseudocode: String variable to message 
+const message = "You have received this message because you have been chosen to open an import vault. Here is the secret combination:";
+//pseudocode: alert to useer 
+alert(message);
+//pseudocode: three variables with +,-,*
+const code1 = 5 + 3;
+const code2 = 7 - 2;
+const code3 = 4 * 4;
+//pseudocode: dispaly vault codes within alert
+alert("Vault Codes: \ncode 1: " + code1 + "\ncode2: " + code2 + "\ncode3: " + code3);
